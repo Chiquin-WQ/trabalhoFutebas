2c3a73ccc419db0e99f4cd9360c89a5cc525aece
eulk21 <lucasptc200@gmail.com>
2026-07-02 23:13:12 -0300

    mudanças criticas

--- trabJoao/repository/CampeonatoRepository.php
+++ trabJoao/repository/CampeonatoRepository.php
@@ -10,32 +10,18 @@ class CampeonatoRepository {
     }
 
     public function listarTodos(): array {
-        $stmt = $this->pdo->query("SELECT * FROM campeonatos ORDER BY ano DESC, nome ASC");
-        $resultados = $stmt->fetchAll();
-
-        $campeonatos = [];
-        foreach ($resultados as $dados) {
-            $campeonatos[] = new Campeonato($dados);
-        }
-
-        return $campeonatos;
+        $stmt = $this->pdo->query("SELECT * FROM campeonatos ORDER BY ano DESC");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function salvar(Campeonato $campeonato): bool {
         $sql = "INSERT INTO campeonatos (nome, ano, premiacao) VALUES (:nome, :ano, :premiacao)";
-        
         $stmt = $this->pdo->prepare($sql);
         
-        $executou = $stmt->execute([
+        return $stmt->execute([
             ':nome'      => $campeonato->getNome(),
             ':ano'       => $campeonato->getAno(),
             ':premiacao' => $campeonato->getPremiacao()
         ]);
-
-        if ($executou) {
-            $campeonato->registrarIdGerado((int)$this->pdo->lastInsertId());
-        }
-
-        return $executou;
     }
 }
