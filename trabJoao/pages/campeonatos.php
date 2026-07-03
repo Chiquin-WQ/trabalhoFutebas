83146faf8ae960400832ec74030eb662035cc4bc
eulk21 <lucasptc200@gmail.com>
2026-07-02 23:17:08 -0300

    mudanças de sofrencia, quero dormir

--- trabJoao/pages/campeonatos.php
+++ trabJoao/pages/campeonatos.php
@@ -1,15 +1,48 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../repository/CampeonatoRepository.php';
 
 $repository = new CampeonatoRepository($pdo);
 $campeonatos = $repository->listarTodos();
 ?>
-
-<?php foreach ($campeonatos as $c): ?>
-    <tr>
-        <td><?= htmlspecialchars($c['nome']) ?></td>
-        <td><?= htmlspecialchars($c['ano']) ?></td>
-        <td><?= htmlspecialchars($c['premiacao']) ?></td>
-    </tr>
-<?php endforeach; ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Campeonatos</title>
+    <link rel="stylesheet" href="../assets/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Campeonatos Cadastrados</h1>
+        <a href="campeonato_create.php" class="btn btn-primary">+ Novo Campeonato</a>
+        <a href="index.php" class="btn btn-ghost">Voltar</a>
+        <br><br>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Ano</th>
+                    <th>Premiação</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php if (empty($campeonatos)): ?>
+                    <tr><td colspan="3">Nenhum campeonato encontrado.</td></tr>
+                <?php else: ?>
+                    <?php foreach ($campeonatos as $c): ?>
+                    <tr>
+                        <td><?= htmlspecialchars($c['nome']) ?></td>
+                        <td><?= htmlspecialchars($c['ano']) ?></td>
+                        <td><?= htmlspecialchars($c['premiacao']) ?></td>
+                    </tr>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
